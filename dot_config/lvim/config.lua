52af4fb5f4bc89235dff376957e88fcfbe2797a9
Muhamad Iqbal <mirfmaster@gmail.com>
2022-07-23 14:29:54 +0700

    adding needed plugin

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,52 +1,44 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.builtin.cmp.completion.keyword_length = 2
-
--- lvim.builtin.treesitter.highlight.enabled = true
--- lvim.builtin.nvimtree.setup.view.side = "right"
--- lvim.builtin.nvimtree.show_icons.git = 1
--- lvim.lsp.diagnostics.virtual_text = true
--- lvim.builtin.nvimtree.hide_dotfiles = 0
+lvim.builtin.nvimtree.hide_dotfiles = false
+-- lvim.builtin.alpha.startify.config
+lvim.lsp.diagnostics.virtual_text = false
 -- to disable icons and use a minimalist setup, uncomment the following
 lvim.use_icons = true
 
 -- unmap a default keymapping
--- vim.keymap.del("n", "<C-e>")
+-- vim.keymap.del("n", "<C-S>")
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<C-q>"] = ":q<cr>]" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-lvim.keys.normal_mode["S"] = ":%s//g<left><left>" -- Search/Replace
+-- lvim.keys.normal_mode["<C-W>"] = ":noa w<cr>"
+lvim.keys.normal_mode["<C-x>"] = ":qa!<cr>]" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- UTILITY
+lvim.keys.normal_mode["<A-r>"] = ":%s//g<left><left>" -- Search/Replace
+vim.api.nvim_set_keymap('i', '<C-s>', [[<Esc><C-s>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-a>', [[ggVG]], { noremap = true, silent = true })
 
 -- NAVIGATION TAB
 lvim.keys.normal_mode["("] = ":bp<cr>"
 lvim.keys.normal_mode[")"] = ":bn<cr>"
 lvim.keys.normal_mode["<Tab>"] = ":bn<cr>"
 lvim.keys.normal_mode["<S-Tab>"] = ":bp<cr>"
-vim.api.nvim_set_keymap('n', '<C-a>', [[ggVG]], { noremap = true, silent = true })
+lvim.keys.normal_mode["<C-S-p>"] = ":Telescope command_center<cr>"
 
 -- NAVIGATION KEYS
-vim.api.nvim_set_keymap('i', '<A-j>', [[<Down>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-k>', [[<Up>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-h>', [[<Left>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-l>', [[<Right>]], { noremap = true, silent = true })
-
--- vim.api.nvim_set_keymap('n', '<C-e>', [[<End>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<C-b>', [[^]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<C-e>', [[<Leader>e]], { noremap = true, silent = true })
-lvim.keys.normal_mode["<C-e>"] = "<Leader>e"
+vim.api.nvim_set_keymap('i', '<A-j>', [[<Down>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-b>', [[^]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-e>', [[$]], { noremap = true, silent = true })
 
 -- override a default keymapping
 -- vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>Telescope find_files<cr>]], { noremap = true, silent = true, expr = true })
@@ -68,7 +60,6 @@ lvim.builtin.telescope.defaults.mappings = {
   },
 }
 
--- lvim.keys.normal_mode["C-p"] = "<space>f"
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<cr>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
@@ -80,12 +71,25 @@ lvim.builtin.which_key.mappings["t"] = {
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
+lvim.builtin.which_key.mappings["T"] = {
+  name = "+T Keyword",
+  W = { "<cmd>Twilight<cr>", "Twilight Toggle" },
+  D = { "<cmd>TwilightDisable<cr>", "Twilight Disable" },
+  E = { "<cmd>TwilightEnable<cr>", "Twilight Enable" },
+  T = { "<cmd>TodoTrouble<cr>", "Todo Trouble" },
+}
+lvim.builtin.which_key.mappings["S"] = {
+  name = "Session",
+  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+}
 
 lvim.builtin.lualine.style = "lvim" -- or "none"
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "startify"
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 
@@ -101,55 +105,15 @@ lvim.builtin.terminal.active = true
 -- }
 lvim.builtin.treesitter.highlight.enabled = false
 
--- -- Prettier configuration
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- vim.lsp.buf.formatting_sync(nil, 3000)
--- formatters.setup({
---   {
---     exe = "prettier",
---     filetypes = {
---       "javascriptreact",
---       "javascript",
---       "typescriptreact",
---       "typescript",
---       "json",
---       "html"
---     },
---   },
--- })
--- ESLint
-
--- local eslint = require("eslint")
--- eslint.setup({
---   bin = 'eslint', -- or `eslint_d`
---   code_actions = {
---     enable = true,
---     apply_on_save = {
---       enable = true,
---       types = { "problem" }, -- "directive", "problem", "suggestion", "layout"
---     },
---     disable_rule_comment = {
---       enable = true,
---       location = "separate_line", -- or `same_line`
---     },
---   },
---   diagnostics = {
---     enable = true,
---     report_unused_disable_directives = false,
---     run_on = "save", -- or `save`
---   },
--- })
-
 -- THEME
--- lvim.colorscheme = "sonokai"
-lvim.colorscheme = "onedarkpro"
--- lvim.colorscheme = "kanagawa"
+lvim.colorscheme = "tokyonight"
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000", }
+-- lvim.colorscheme = "onedarkpro"
 
 -- Additional Plugins
 lvim.plugins = {
-  -- { "sainnhe/sonokai" },
-  -- { "rebelot/kanagawa.nvim" },
-  -- { 'MunifTanjim/eslint.nvim' },
+  { 'folke/tokyonight.nvim', },
   {
     "olimorris/onedarkpro.nvim",
     config = function()
@@ -164,21 +128,25 @@ lvim.plugins = {
   { 'mg979/vim-visual-multi' },
   { 'neoclide/coc.nvim', branch = 'release' },
   { "lukas-reineke/indent-blankline.nvim" },
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function() require "lsp_signature".on_attach() end,
-  -- },
-  -- {
-  --   "sindrets/diffview.nvim",
-  --   event = "BufRead",
-  -- },
-  -- {
-  --   "f-person/git-blame.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     vim.cmd "highlight default link gitblame SpecialComment"
-  --     vim.g.gitblame_enabled = 1
-  --   end,
-  -- },
+  { 'farmergreg/vim-lastplace' },
+  { "folke/twilight.nvim" },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup {
+        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+      }
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end
+  }
 }
