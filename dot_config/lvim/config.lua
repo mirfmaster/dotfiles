197471e9a0a0c5a00ae4752ebd81c0f1a0937fe9
Muhamad Iqbal <mirfmaster@gmail.com>
2023-07-31 11:19:06 +0700

    feat: adjustment on lvim config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,11 +1,15 @@
 -- INIT
 vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
+local map = vim.api.nvim_set_keymap
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
+local lazygit = Terminal:new { cmd = "lazygit", hidden = true, direction = "float" }
 
+---@diagnostic disable-next-line: lowercase-global
 function _lazygit_toggle() lazygit:toggle() end
 
--- general
+map("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc = 'Lazygit integration' })
+
+-- GLOBAL GENERAL CONFIG
 lvim.leader = "space"
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
@@ -13,78 +17,69 @@ lvim.format_on_save.enabled = true
 vim.opt.relativenumber = true
 vim.opt.wrap = true
 
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
--- lvim.keys.normal_mode["<S-j>"] = ":tabprev<CR>"
--- lvim.keys.normal_mode["<S-k>"] = ":tabnext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":tabmove -1<CR>"
--- lvim.keys.normal_mode["<S-l>"] = ":tabmove +1<CR>"
-local map = vim.api.nvim_set_keymap
+vim.g.codeium_no_map_tab = true
+
 -- BIND NEW KEYMAPS
 -- UTILITIES
-map("i", "<C-s>", [[<Esc><C-s>]], { noremap = true, silent = true })
-map("n", "<S-s>", ":noa w<cr>", { noremap = true, silent = true, desc = 'Save without formatting' })
-map("n", "<A-s>", ":noa w<cr>", { noremap = true, silent = true, desc = 'Save without formatting' })
-map("i", "<C-c>", [[<Esc>]], { noremap = true, silent = true })
-map("n", "<C-a>", [[ggVG]], { noremap = true, silent = true })
+map("i", "<C-s>", [[<Esc><C-s>]], { noremap = true, silent = true, desc = "Save" })
+map("i", "<C-c>", [[<Esc>]], { noremap = true, silent = true, desc = "Exit insert mode" })
 map("i", "<C-BS>", [[<C-w>]], { noremap = true, silent = false, desc = 'Ctrl Backspace' })
-map("n", "<C-a>", [[ggVG]], { noremap = true, silent = true })
-map("n", "<C-.>", ":Telescope session-lens search_session<cr>", { noremap = true, silent = true, desc = 'Open session' })
 map("i", "<S-Tab>", [[<C-d>]], { noremap = true, silent = false })
 
--- map("n", "<f8>", ":cprev<cr>", { desc = "Previous item in quickfix list" })
--- map("n", "<f9>", ":cnext<cr>", { desc = "Next item in quickfix list" })
-map("n", "<M-CR>", ":lua vim.lsp.buf.code_action()<CR>", {})
+map("n", "<C-s>", ":w<cr>", { noremap = true, silent = true, desc = "Save" })
+map("n", "<S-l>", ":BufferLineCycleNext<cr>", { noremap = true, silent = true, desc = "Next buffer" })
+map("n", "<S-h>", ":BufferLineCyclePrev<cr>", { noremap = true, silent = true, desc = "Prev buffer" })
+map("n", "<S-s>", ":noa w<cr>", { noremap = true, silent = true, desc = 'Save without formatting' })
+map("n", "<A-s>", ":noa w<cr>", { noremap = true, silent = true, desc = 'Save without formatting' })
+map("n", "<C-.>", ":Telescope session-lens search_session<cr>", { noremap = true, silent = true, desc = 'Open session' })
+map("n", "<C-a>", [[ggVG]], { noremap = true, silent = true, desc = "Select all" })
+map("n", "<M-CR>", ":lua vim.lsp.buf.code_action()<CR>", { desc = "Code action, <Alt-Enter> to use" })
+map("n", "<C-p>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 -- map("n", "<leader>qf", ":lua hu_toggle_qf()<cr>", { desc = "Toggle quickfix list" })
 
 -- Navigation tab
 -- map("n", "<C-w><C-t>", ":tabnew<cr>", { desc = "Open new tab (?)" })
 -- map("n", "<C-w><C-q>", ":tabc<cr>", { desc = "Close current tab" })
-map("n", "<C-w><C-h>", ":tabp<cr>", { desc = "Prevous tab" })
-map("n", "<C-w><C-l>", ":tabn<cr>", { desc = "Next tab" })
+map("n", "<C-w><C-h>", ":tabp<cr>", { noremap = true, silent = true, desc = "Prevous tab" })
+map("n", "<C-w><C-l>", ":tabn<cr>", { noremap = true, silent = true, desc = "Next tab" })
 
-map("n", "<C-w><C-t>", ":terminal<cr>", { desc = "Make it as terminal" })
-map("n", "<C-w><C-a>", ":tabnew<cr>", { desc = "Open new tab (?)" })
-map("n", "<C-w><C-q>", ":tabc<cr>", { desc = "Close current tab" })
-map("n", "<C-9>", ":tabp<cr>", { desc = "Prevous tab" })
-map("n", "<C-0>", ":tabn<cr>", { desc = "Next tab" })
+map("n", "<C-w><C-t>", ":terminal<cr>", { noremap = true, silent = true, desc = "Make it as terminal" })
+map("n", "<C-w><C-a>", ":tabnew<cr>", { noremap = true, silent = true, desc = "Open new tab (?)" })
+map("n", "<C-w><C-q>", ":tabc<cr>", { noremap = true, silent = true, desc = "Close current tab" })
+map("n", "<C-9>", ":tabp<cr>", { noremap = true, silent = true, desc = "Prevous tab" })
+map("n", "<C-0>", ":tabn<cr>", { noremap = true, silent = true, desc = "Next tab" })
 
 -- NAVIGATION KEYS
-map("i", "<C-k>", [[<Up>]], { noremap = true, silent = true })
-map("i", "<C-h>", [[<Left>]], { noremap = true, silent = true })
-map("i", "<C-l>", [[<Right>]], { noremap = true, silent = true })
-map("i", "<C-j>", [[<Down>]], { noremap = true, silent = true })
+map("i", "<C-k>", [[<Up>]], { noremap = true, silent = true, desc = "Equivalent arrow Up" })
+map("i", "<C-h>", [[<Left>]], { noremap = true, silent = true, desc = "Equivalent arrow Left" })
+map("i", "<C-l>", [[<Right>]], { noremap = true, silent = true, desc = "Equivalent arrow Right" })
+map("i", "<C-j>", [[<Down>]], { noremap = true, silent = true, desc = "Equivalent arrow Down" })
 
-map("n", "<S-b>", [[^]], { noremap = true, silent = true })
-map("n", "<S-e>", [[$]], { noremap = true, silent = true })
--- map("n", "F", ":HopChar2<cr>", { silent = true })
-map("n", "f", ":HopChar2<cr>", { silent = true })
--- map("n", "f", ":HopWord<cr>", { silent = true })
+map("n", "<S-b>", [[^]], { noremap = true, silent = true, desc = "Go to beginning" })
+map("n", "<S-e>", [[$]], { noremap = true, silent = true, desc = "Go to end" })
 
--- map('n', '<C-p>', ":lua require'telescope'.extensions.project.project{}<CR>", { noremap = true, silent = true })
+map("n", "F", ":HopChar2<cr>", { noremap = true, silent = true, })
+map("n", "f", ":HopWord<cr>", { noremap = true, silent = true, })
 
 -- TERMINAL
-map("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>",
-    { noremap = true, silent = true, desc = 'Lazygit terminal integration' })
 
-map("n", "<C-t>", [[:ToggleTerm<cr>]], { noremap = true, silent = true })
--- map("n", "<C-t><C-k>", [[:ToggleTerm direction=float<cr>]], { noremap = true, silent = true })
--- map("n", "<C-t><C-k>", [[:ToggleTerm direction=horizontal<cr>]], { noremap = true, silent = true })
--- map("n", "<C-t><C-l>", [[:ToggleTerm direction=vertical size=80<cr>]], { noremap = true, silent = true })
+map("n", "<C-t>", [[:ToggleTerm direction=float size=100<cr>]], { noremap = true, silent = true })
+-- map("n", "<F3>", [[:ToggleTerm direction=vertical size=200<cr>]], { noremap = true, silent = true })
 
 -- TOGGLETERM
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set("t", "<C-t>", [[<Cmd>ToggleTerm<CR>]], opts)
+  vim.keymap.set("t", "<F3>", [[<Cmd>ToggleTerm<CR>]], opts)
 
   vim.keymap.set("t", "<C-Space>", [[<C-\><C-n>]], opts) -- esc to normal mode
+  vim.keymap.set("t", "<C-;>", [[<C-\><C-n>]], opts) -- esc to normal mode
   vim.keymap.set("t", "<C-j>", [[<Down>]], opts)
   vim.keymap.set("t", "<C-k>", [[<Up>]], opts)
-  -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Left>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Right>]], opts)
+  vim.keymap.set("t", "<C-,>", [[<C-Left>]], opts)
+  vim.keymap.set("t", "<C-.>", [[<C-Right>]], opts)
   -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
   -- vim.keymap.set("t", "<S-h>", [[<Cmd>BufferLineCyclePrev<CR>]], opts)
   -- vim.keymap.set("t", "<S-l>", [[<Cmd>BufferLineCycleNext<CR>]], opts)
@@ -101,8 +96,7 @@ end
 -- override a default keymapping
 lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- TELESCOPE ACTIONS
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
     -- for input mode
@@ -132,13 +126,13 @@ lvim.builtin.which_key.mappings["t"] = {
     t = { "<cmd>TodoTelescope<cr>", "Workspace TODO" },
 }
 -- lvim.builtin.which_key.mappings['h'] = {}
-lvim.builtin.which_key.mappings[';'] = {}
-lvim.builtin.which_key.mappings[";"] = {
-    name = "+Harpoon",
-    a = { ":lua require('harpoon.mark').add_file()<cr>", "Add Mark File" },
-    d = { ":lua require('harpoon.mark').delete_file()<cr>", "Delete Mark File" },
-    [";"] = { ":lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle Quick Menu" },
-}
+-- lvim.builtin.which_key.mappings[';'] = {}
+-- lvim.builtin.which_key.mappings[";"] = {
+--     name = "+Harpoon",
+--     a = { ":lua require('harpoon.mark').add_file()<cr>", "Add Mark File" },
+--     d = { ":lua require('harpoon.mark').delete_file()<cr>", "Delete Mark File" },
+--     [";"] = { ":lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle Quick Menu" },
+-- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -148,20 +142,15 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
--- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
     "javascript",
     "json",
-    "lua",
     "typescript",
     "tsx",
     "css",
     "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
 -- generic LSP settings
@@ -210,8 +199,8 @@ linters.setup {
 vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
 -- Theme
-lvim.colorscheme = "tokyonight-moon"
--- lvim.colorscheme = "tokyodark"
+-- lvim.colorscheme = "tokyonight-moon"
+lvim.colorscheme = "tokyodark"
 -- vim.colorscheme = "substrata"
 -- lvim.colorscheme = "carbonfox"
 
@@ -276,21 +265,13 @@ lvim.plugins = {
     },
     {
         "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-          require("todo-comments").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-        end
+        requires = "nvim-lua/plenary.nvim"
     },
     { 'godlygeek/tabular', },
     -- { 'ThePrimeagen/harpoon' },
     {
         'Exafunction/codeium.vim',
         config = function()
-          -- Change '<C-g>' here to any keycode you like.
           vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#Accept']() end, { expr = true })
           vim.keymap.set('i', '<c-.>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
           vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions']( -1) end, { expr = true })
