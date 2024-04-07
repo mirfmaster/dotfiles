c4b845bd91e5f57824f99ae7924acaa6ac680a8f
Muhamad Iqbal <mirfmaster@gmail.com>
2024-04-07 15:54:45 +0700

    set silent and noremap as defaultOpts

--- dot_config/lvim/lua/user/keymap.lua
+++ dot_config/lvim/lua/user/keymap.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap.set
 
-DefaultOpts = {
+local defaultOpts = {
   noremap = true,
   silent = true,
 }
@@ -18,37 +18,33 @@ lvim.keys.normal_mode = {
   ["<C-w><C-t>"] = { "<NOP>", desc = "Open terminal" },
   ["<C-w><C-a>"] = { "<NOP>", desc = "Open new tab" },
   ["<C-w><C-q>"] = { "<NOP>", desc = "Close current tab" },
-  ["<F1>"] = { "<NOP>", desc = "Help", DefaultOpts },
 }
 
-keymap("n", "<C-w><C-v>", "<NOP>", DefaultOpts)
-keymap("n", "<C-w><C-s>", "<NOP>", DefaultOpts)
--- vim.api.nvim_del_keymap('n', '<C-w><C-v>')
--- -- Disable <C-w><C-s>
--- vim.api.nvim_del_keymap('n', '<C-w><C-s>')
--- vim.api.nvim_del_keymap('n', '<F1>')
+keymap("n", "<C-w><C-v>", "<NOP>", defaultOpts)
+keymap("n", "<C-w><C-s>", "<NOP>", defaultOpts)
+keymap("n", "<F1>", "<NOP>", defaultOpts)
 -----------------------------------------------------------
 
 -- Normal mode key mappings
 lvim.keys.normal_mode = {
   -- NVIM management
-  ["<C-x>"] = { ":qa<CR>", desc = "Close buffer", DefaultOpts },
-  ["<C-q>"] = { ":q<CR>", desc = "Close buffer", DefaultOpts },
-  ["<M-q>"] = { ":q<CR>", desc = "Close buffer", DefaultOpts },
+  ["<C-x>"] = { ":qa<CR>", desc = "Close buffer", defaultOpts },
+  ["<C-q>"] = { ":q<CR>", desc = "Close buffer", defaultOpts },
+  ["<M-q>"] = { ":q<CR>", desc = "Close buffer", defaultOpts },
 
   -- File operations
-  ["<C-s>"] = { ":w<cr>", desc = "Save", DefaultOpts },
-  ["<C-S-s>"] = { ":noa w<cr>", desc = "Save without formatting", DefaultOpts },
+  ["<C-s>"] = { ":w<cr>", desc = "Save", defaultOpts },
+  ["<C-S-s>"] = { ":noa w<cr>", desc = "Save without formatting", defaultOpts },
 
   -- Git integration
-  ["<C-g>"] = { "<cmd>lua _lazygit_toggle()<CR>", desc = "Lazygit integration", DefaultOpts },
+  ["<C-g>"] = { "<cmd>lua _lazygit_toggle()<CR>", desc = "Lazygit integration", defaultOpts },
 
   -- Buffer navigation
-  ["<S-l>"] = { ":BufferLineCycleNext<cr>", desc = "Next buffer", DefaultOpts },
-  ["<S-h>"] = { ":BufferLineCyclePrev<cr>", desc = "Prev buffer", DefaultOpts },
-  ["<C-p>"] = { "<cmd>Telescope find_files<CR>", desc = "Find files", DefaultOpts },
-  ["<C-S-p>"] = { "<cmd>Telescope projects<CR>", desc = "Projects", DefaultOpts },
-  ["F"] = { ":Telescope buffers<cr>", desc = "Search buffers", DefaultOpts },
+  ["<S-l>"] = { ":BufferLineCycleNext<cr>", desc = "Next buffer", defaultOpts },
+  ["<S-h>"] = { ":BufferLineCyclePrev<cr>", desc = "Prev buffer", defaultOpts },
+  ["<C-p>"] = { "<cmd>Telescope find_files<CR>", desc = "Find files", defaultOpts },
+  ["<C-S-p>"] = { "<cmd>Telescope projects<CR>", desc = "Projects", defaultOpts },
+  ["F"] = { ":Telescope buffers<cr>", desc = "Search buffers", defaultOpts },
 
   -- Tab management
   -- ["<C-w><C-h>"] = { ":tabp<cr>", desc = "Previous tab" },
@@ -58,65 +54,64 @@ lvim.keys.normal_mode = {
   -- ["<C-w><C-q>"] = { ":tabc<cr>", desc = "Close current tab" },
   -- NOTE: currently trying to use C@M config
   -- Tab management
-  ["<M-t>"] = { ":tabnew<cr>", desc = "Open new tab" },
-  ["<M-w>"] = { ":tabc<cr>", desc = "Close current tab" },
-  ["<M-r>"] = { ":terminal<cr>", desc = "Open terminal" },
-  ["<M-h>"] = { ":tabp<cr>", desc = "Previous tab" },
-  ["<M-l>"] = { ":tabn<cr>", desc = "Next tab" },
+  ["<M-t>"] = { ":tabnew<cr>", desc = "Open new tab", defaultOpts },
+  ["<M-w>"] = { ":tabc<cr>", desc = "Close current tab", defaultOpts },
+  ["<M-r>"] = { ":terminal<cr>", desc = "Open terminal", defaultOpts },
+  ["<M-h>"] = { ":tabp<cr>", desc = "Previous tab", defaultOpts },
+  ["<M-l>"] = { ":tabn<cr>", desc = "Next tab", defaultOpts },
   -- Window
   ["<M-s>"] = { ":split<cr>", desc = "Split window" },
   ["<M-v>"] = { ":vsplit<cr>", desc = "Vsplit window" },
 
   -- Window resize
-  ["<Up>"] = { ":resize +5<cr>", desc = "Upsize", DefaultOpts },
-  ["<Down>"] = { ":resize -5<cr>", desc = "Downsize", DefaultOpts },
-  ["<Left>"] = { ":vertical resize -5<cr>", desc = "Vertical resize -5", DefaultOpts },
-  ["<Right>"] = { ":vertical resize +5<cr>", desc = "Vertical resize +5", DefaultOpts },
+  ["<Up>"] = { ":resize +5<cr>", desc = "Upsize", defaultOpts },
+  ["<Down>"] = { ":resize -5<cr>", desc = "Downsize", defaultOpts },
+  ["<Left>"] = { ":vertical resize -5<cr>", desc = "Vertical resize -5", defaultOpts },
+  ["<Right>"] = { ":vertical resize +5<cr>", desc = "Vertical resize +5", defaultOpts },
 
   -- Text and content manipulation
-  ["<C-a>"] = { "ggVG", desc = "Select all", DefaultOpts },
-  ["<M-CR>"] = { ":lua vim.lsp.buf.code_action()<CR>", desc = "Code action", DefaultOpts },
+  ["<C-a>"] = { "ggVG", desc = "Select all", defaultOpts },
+  ["<M-CR>"] = { ":lua vim.lsp.buf.code_action()<CR>", desc = "Code action", defaultOpts },
 
   -- HARPOON
-  -- ["<C-h>"] = { ':lua require("harpoon.ui").toggle_quick_menu()<cr>' },
-  [";r"] = { ':lua require("harpoon.ui").toggle_quick_menu()<cr>' },
-  [";a"] = { ':lua require("harpoon.mark").add_file()<cr>' },
-  [";s"] = { ":Telescope session-lens search_session<cr>", DefaultOpts },
+  [";r"] = { ':lua require("harpoon.ui").toggle_quick_menu()<cr>', defaultOpts },
+  [";a"] = { ':lua require("harpoon.mark").add_file()<cr>', defaultOpts },
+  [";s"] = { ":Telescope session-lens search_session<cr>", defaultOpts },
 
   -- Others
-  ["gR"] = { "<cmd>Trouble lsp_references<CR>", desc = "LSP References", DefaultOpts },
-  ["f"] = { ":HopChar2<cr>", desc = "Hop", DefaultOpts },
+  ["gR"] = { "<cmd>Trouble lsp_references<CR>", desc = "LSP References", defaultOpts },
+  ["f"] = { ":HopChar2<cr>", desc = "Hop", defaultOpts },
 
-  -- ["gl"] = { ":Glow<cr>", desc = "Glow - Markdown preview", DefaultOpts },
+  -- ["gl"] = { ":Glow<cr>", desc = "Glow - Markdown preview", defaultOpts },
 }
 
 -- Insert mode key mappings
 lvim.keys.insert_mode = {
   -- File operations
-  ["<C-s>"] = { "<Esc>:w<CR>", desc = "Save while insert mode" },
+  ["<C-s>"] = { "<Esc>:w<CR>", desc = "Save while insert mode", defaultOpts },
 
-  ["<C-c>"] = { "<Esc>", desc = "Exit insert mode" },
+  ["<C-c>"] = { "<Esc>", desc = "Exit insert mode", defaultOpts },
 
   -- Text manipulation
-  ["<C-BS>"] = { "<C-w>", desc = "Ctrl Backspace" },
-  ["<S-Tab>"] = { "<C-d>", desc = "Shift Tab" },
+  ["<C-BS>"] = { "<C-w>", desc = "Ctrl Backspace", defaultOpts },
+  ["<S-Tab>"] = { "<C-d>", desc = "Shift Tab", defaultOpts },
 
   -- Cursor movement
-  ["<C-k>"] = { "<Up>", desc = "Move cursor up" },
-  ["<C-h>"] = { "<Left>", desc = "Move cursor left" },
-  ["<C-l>"] = { "<Right>", desc = "Move cursor right" },
-  ["<C-j>"] = { "<Down>", desc = "Move cursor down" }
+  ["<C-k>"] = { "<Up>", desc = "Move cursor up", defaultOpts },
+  ["<C-h>"] = { "<Left>", desc = "Move cursor left", defaultOpts },
+  ["<C-l>"] = { "<Right>", desc = "Move cursor right", defaultOpts },
+  ["<C-j>"] = { "<Down>", desc = "Move cursor down", defaultOpts }
 }
 
 
 lvim.keys.visual_mode = {
   -- Better indenting
-  ["<"] = { "<gv" },
-  [">"] = { ">gv" },
+  ["<"] = { "<gv", defaultOpts },
+  [">"] = { ">gv", defaultOpts },
 
   -- -- Paste most recent yank
-  ["p"] = { '"0p', { silent = true } },
-  ["P"] = { '"0P', { silent = true } },
+  ["p"] = { '"0p', defaultOpts },
+  ["P"] = { '"0P', defaultOpts },
 }
 
 lvim.keys.term_mode = {
@@ -153,24 +148,23 @@ lvim.keys.term_mode = {
 }
 
 -- C@Machine config
-local opts = DefaultOpts
-keymap("n", "<m-h>", "<C-w>h", opts)
-keymap("n", "<m-j>", "<C-w>j", opts)
-keymap("n", "<m-k>", "<C-w>k", opts)
-keymap("n", "<m-l>", "<C-w>l", opts)
+keymap("n", "<m-h>", "<C-w>h", defaultOpts)
+keymap("n", "<m-j>", "<C-w>j", defaultOpts)
+keymap("n", "<m-k>", "<C-w>k", defaultOpts)
+keymap("n", "<m-l>", "<C-w>l", defaultOpts)
 
--- keymap("n", "<c-j>", "<c-d>", opts)
--- keymap("n", "<c-k>", "<c-u>", opts)
-keymap("n", "<c-m>", "<s-m>", opts)
+-- keymap("n", "<c-j>", "<c-d>", defaultOpts)
+-- keymap("n", "<c-k>", "<c-u>", defaultOpts)
+keymap("n", "<c-m>", "<s-m>", defaultOpts)
 
-keymap("n", "n", "nzz", opts)
-keymap("n", "N", "Nzz", opts)
-keymap("n", "*", "*zz", opts)
-keymap("n", "#", "#zz", opts)
-keymap("n", "g*", "g*zz", opts)
-keymap("n", "g#", "g#zz", opts)
+keymap("n", "n", "nzz", defaultOpts)
+keymap("n", "N", "Nzz", defaultOpts)
+keymap("n", "*", "*zz", defaultOpts)
+keymap("n", "#", "#zz", defaultOpts)
+keymap("n", "g*", "g*zz", defaultOpts)
+keymap("n", "g#", "g#zz", defaultOpts)
 
-keymap("n", "<C-z>", "<cmd>ZenMode<cr>", opts)
+keymap("n", "<C-z>", "<cmd>ZenMode<cr>", defaultOpts)
 
-keymap("n", "<m-f>", ":call QuickFixToggle()<cr>", opts)
-keymap("n", "<C-i>", "<C-i>", opts)
+keymap("n", "<m-f>", ":call QuickFixToggle()<cr>", defaultOpts)
+keymap("n", "<C-i>", "<C-i>", defaultOpts)
